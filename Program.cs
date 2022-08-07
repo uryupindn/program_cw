950eb447d476ea16a5748769bc6495cc94c00caa
uryupindn <uryupindn@gmail.com>
2022-08-07 22:13:30 +0300

    добавил первую часть кода

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите элементы массива через пробел: ");
+string[] array=GetArrayFromString(ReadLine());
+WriteLine($"[{string.Join(", ", array)}]");
+
+string[] GetArrayFromString(string array) //1) задаем функцию, которая инициализирует массив из строки
+{
+    string[] arS=array.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    string[] result=new string[arS.Length];
+    for(int i=0; i<arS.Length;i++)
+    {
+        result[i]=Convert.ToString(arS[i]);
+    }
+    return result;
